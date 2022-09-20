b2e32cb84955beee6c5c70125c98b3e643a9d14e
Petar1304 <kovacevic1304@gmail.com>
2022-09-20 22:08:19 +0200

    done

--- bank.py
+++ bank.py
@@ -1,13 +1,13 @@
-import serial 
+import serial
 from datetime import datetime
-from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QLineEdit, QWidget # QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QWidget  # QVBoxLayout
 import sys
-from PyQt5.QtCore import QObject, QThread, pyqtSignal
+from PyQt5.QtCore import QObject, QThread, pyqtSignal # , pyqtSlot
 
 # path variables
-PORT = 'dev/tty/ACM0'
-IN_FILE_PATH = '/home/petar/Documents/pms/IDbaza.txt'
-OUT_FILE_PATH = '/home/petar/Documents/pms/provalnici.txt'
+PORT = 'COM6'#'dev/tty/ACM0'
+IN_FILE_PATH = 'C:/Users/marko/Downloads/pera/PM/sketch_jun23a/IDbaza.txt'
+OUT_FILE_PATH = 'C:/Users/marko/Downloads/pera/PM/sketch_jun23a/provalnici.txt'
 
 class Worker(QObject):
     # add all signals
@@ -20,7 +20,7 @@ class Worker(QObject):
         self.num_people = 0
         self.ids = self.load_ids()
         self.known_id = False
-    
+
     def open_serial(self):
         com_port = PORT
         baud_rate = 9600
@@ -42,7 +42,7 @@ class Worker(QObject):
         with open(file, 'a') as f:
             f.write(f'Time {current_time} => ID: {ID}\n')
 
-    # id, distance, touch, volts    
+    # id, distance, touch, volts
     def process_data(self, data):
         l = data.strip().split(',')
         data_dict = dict()
@@ -75,52 +75,60 @@ class Worker(QObject):
         ser = self.open_serial()
         # sva vrate su zatvorena na pocetku
         first_door = False
+        old_id = ''
 
         try:
             # add variable for exiting loop and program
             while True:
                 arduino_data = ser.readline().decode()
                 data_dict = self.process_data(arduino_data)
-                
+
                 print(data_dict)
 
                 # calculating max ppl in bank
-                max_people_in_bank = data_dict['voltage'] * 4
+                max_people_in_bank = int(data_dict['voltage']) * 4
 
                 # provera da li su prva vrata zatvorena
-                if data_dict['distance'] < 10:
+                if data_dict['distance'] <= 10:
                     first_door = False
                 else:
                     first_door = True
 
                 # provera dodira
-                if data_dict['touch'] == 1 and self.known_id and first_door == False:
+                if data_dict['touch'] == 1 and (not first_door) and (self.num_people < max_people_in_bank): #   and self.known_id: #  and self.known_id
                     # ako su prva vrata zatvorena i sensor je dodirnut druga vrata se otvaraju
                     self.num_people += 1
                     self.entered_bank.emit()
                     data_out['led_green'] = 1
+                    data_out['led_red'] = 0
+                    old_id = data_dict['id']
 
                 # check id
                 if data_dict['id'] != '0':
+                    # if data_dict['id'] != '0':
                     self.changed_id.emit(data_dict['id'])
                     # gasi se zelena lampica
-                    data_out['led_green'] = 0
-                    data_out['led_red'] = 0
+                    if data_dict['id'] != old_id:
+                        data_out['led_green'] = 0
+                        data_out['led_red'] = 0
 
                     if data_dict['id'] in self.ids:
-                        print('poznat')
+                        # print('poznat')
                         self.known_id = True
                         # I vrata se otvaraju
                         if self.num_people < max_people_in_bank:
                             first_door = True
+                        else:
+                            first_door = False
                     # upis nepoznatog id-a u fajl
                     else:
                         self.known_id = False
                         self.file_write(data_dict['id'])
+                        data_out['led_green'] = 0
                         data_out['led_red'] = 1
 
                 print(data_out)
-                print(self.num_people)
+                # print(self.num_people)
                 # write to arduino
                 data_out['display'] = self.num_people
                 send_data = self.process_data_out(data_out)
@@ -133,7 +141,8 @@ class Worker(QObject):
         ser.close()
 
 # GUI
-class App(QWidget):
+# class App(QWidget):
+class App(QMainWindow):
     exited_bank = pyqtSignal()
 
     def __init__(self):
@@ -161,7 +170,7 @@ class App(QWidget):
         self.text_input.resize(200, 50)
         self.text_input.move(100, 100)
 
-        # code submit button 
+        # code submit button
         self.exit_button = QPushButton('IZLAZ', self)
         self.exit_button.resize(100, 50)
         self.exit_button.move(400, 100)
@@ -181,7 +190,7 @@ class App(QWidget):
         self.quit_btn = QPushButton('EXIT', self)
         self.quit_btn.resize(100, 50)
         self.quit_btn.move(250, 320)
-        self.quit_btn.clicked.connect(QApplication.instance().quit) # closing app
+        self.quit_btn.clicked.connect(QApplication.instance().quit)  # closing app
         # self.quit_btn.clicked.connect(self.close_app)
         self.show()
 
@@ -198,33 +207,31 @@ class App(QWidget):
 
     # CODE = 8888
     def exit_bank(self):
-        if self.num_people > -10:
+        if self.num_people > -1:
             code = self.text_input.text()
             if code == '8888':
                 # emit code signal
                 self.num_people -= 1
                 self.exited_bank.emit()
+                # Worker.people_sub()
+
                 self.num_indicator.setText(f'Broj ljudi u banci: {self.num_people}')
                 self.notification.setText('')
             else:
                 self.notification.setText('Neispravan Kod')
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
-
     thread = QThread()
-
-    worker = Worker()
-    
+    worker = Worker()   
     worker.moveToThread(thread)
-    thread.started.connect(worker.run)
-
     worker.changed_id.connect(ex.change_id)
     worker.entered_bank.connect(ex.people_add)
-
-    ex.exited_bank.connect(worker.people_sub)
+    thread.started.connect(worker.run)
     thread.start()
+    ## fix
+    # ex.exited_bank.connect(worker.people_sub)
 
-    ex.show() 
+    ex.show()
     app.exec_()
